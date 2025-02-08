92a811ce74fe285392e6282ffee680fca6325cf1
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-02-08 22:25:05 +0300

    Update Register Page

--- lib/TEST1/register.dart
+++ lib/TEST1/register.dart
@@ -74,16 +74,7 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Theme.of(context).primaryColor,
-              Theme.of(context).primaryColor.withOpacity(0.5),
-            ],
-          ),
-        ),
+        color: Theme.of(context).primaryColor,
         child: SafeArea(
           child: Center(
             child: SingleChildScrollView(
